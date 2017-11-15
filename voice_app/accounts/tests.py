ed9bf8f5d60a11fcf649c696f0c85e49ea98fb84
Ash Ramesh <ashramesh1992@gmail.com>
2017-11-15 11:16:22 +1100

    Add unit tests for accounts api

--- voice_app/accounts/tests.py
+++ voice_app/accounts/tests.py
@@ -1,3 +1,36 @@
+from django.db import IntegrityError
 from django.test import TestCase
 
-# Create your tests here.
+from voice_app.accounts.api import create_mentor
+from voice_app.accounts.models import User
+
+
+class APITestCase(TestCase):
+
+    def test_create_mentor(self):
+        mentor = create_mentor('TestMentor', 'Test',
+                               'Mentor', 'password')
+
+        mentor_id = mentor.id
+
+        retrieved_mentor = User.objects.get(pk=mentor_id)
+
+        self.assertEqual(mentor.username,
+                         retrieved_mentor.username)
+
+        self.assertEqual(mentor.first_name,
+                         retrieved_mentor.first_name)
+
+        self.assertEqual(mentor.last_name,
+                         retrieved_mentor.last_name)
+
+        self.assertEqual(mentor.is_mentor, True)
+
+    def test_duplicate_mentor_creation(self):
+
+        create_mentor('TestMentor', 'Test',
+                      'Mentor', 'password')
+
+        with self.assertRaises(IntegrityError) as e:
+            create_mentor('TestMentor', 'Test',
+                          'Mentor', 'password')
